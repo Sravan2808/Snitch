b1f024ae9fde25d62f0b6a53744e6c1386e5b95c
Sravan Surya <vsravansurya@gmail.com>
2026-04-22 13:23:28 +0530

    feat: initialize frontend with React, Redux, and Tailwind CSS
    - Add package.json for project dependencies and scripts
    - Create favicon and icons SVG files for branding
    - Implement main App component with basic structure
    - Set up Redux store with authentication slice
    - Create authentication hooks and API service for user registration
    - Add global CSS file for Tailwind CSS integration
    - Configure Vite for development and build processes

---
+++ Frontend/src/features/auth/hook/useAuth.js
@@ -0,0 +1,18 @@
+import { setError,setLoading,setUser } from "../state/auth.slice";
+import { register } from "../service/auth.api";
+import { useDispatch } from "react-redux";
+
+export const useAuth = ()=>{
+    const dispatch = useDispatch();
+    async function handleRegister({email,contact,password,fullName,isSeller=false}){
+        try {
+            dispatch(setLoading(true));
+            const data = await register({email,contact,password,fullName,isSeller})
+            dispatch(setUser(data.user));
+            dispatch(setLoading(false));
+        } catch (error) {
+            dispatch(setError(error.message));
+            dispatch(setLoading(false));
+        }
+    }
+}
